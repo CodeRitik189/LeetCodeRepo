bf2c2b55571039d0c3f1f56df48bf926fdf25464
Ritik Tripathi <88445142+CodeRitik189@users.noreply.github.com>
2022-02-19 10:31:07 +0530

    Time: 871 ms (7.69%), Space: 10.3 MB (66.98%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -8,6 +8,7 @@ public:
                  if( nums[ i ]+ nums[ j ] == target){
                       v.push_back( i );
                      v.push_back( j );
+                     break;
                  }
             }
         }
