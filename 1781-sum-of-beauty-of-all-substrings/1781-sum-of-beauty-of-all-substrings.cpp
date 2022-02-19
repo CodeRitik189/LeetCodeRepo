543af5b9daffb01e586d409601f07f7d5bfe0fc5
Ritik Tripathi <88445142+CodeRitik189@users.noreply.github.com>
2022-02-19 20:22:53 +0530

    Time: 544 ms (40.61%), Space: 8.8 MB (48.79%) - LeetHub

---
+++ 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int beautySum(string s) {
+        int n=s.size(),sum=0;
+        for(int i=0;i<n;i++){
+            vector<int>v(26,0);
+            for(int j=i;j<n;j++){
+               int mn = INT_MAX,mx = INT_MIN;
+               v[s[j]-'a']++;
+               for(int k=0;k<26;k++){
+                   if(v[k]!=0){
+                    mn=min(mn,v[k]);
+                    mx=max(mx,v[k]);
+                   }
+               }
+               sum += (mx-mn); 
+            }
+        }
+        return sum;
+    }
+};
