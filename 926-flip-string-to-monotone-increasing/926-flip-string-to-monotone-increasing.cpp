6033c6078ece7179f069e3d2deb5296f78cea645
Ritik Tripathi <88445142+CodeRitik189@users.noreply.github.com>
2022-02-28 11:56:33 +0530

    Time: 28 ms (85.10%), Space: 11.1 MB (55.44%) - LeetHub

--- 926-flip-string-to-monotone-increasing/926-flip-string-to-monotone-increasing.cpp
+++ 926-flip-string-to-monotone-increasing/926-flip-string-to-monotone-increasing.cpp
@@ -1,18 +1,13 @@
 class Solution {
 public:
+    //at any point '1' comapare with right 0's and left 1's
     int minFlipsMonoIncr(string s) {
-         int st=0,ans=s.size();
-         vector<int>v(s.size());
+         int st=0,ct=0,ans=s.size();
+         for(int i=0;i<s.size();i++){st += (s[i]=='0');}
          for(int i=0;i<s.size();i++){
-             v[i]= st;
-             st += (s[i]=='0');
-         }
-         for(int i=0;i<s.size();i++){
-             if(s[i]=='1'&&ans>(st-v[i]+i-v[i])){
-                 ans = st-v[i]+i-v[i];
-             }else if(s[i]=='0'&&ans>(st-v[i]+i-v[i]-1)){
-                 ans = st-v[i]+i-v[i]-1;
-             }
+             if(s[i]=='1'&&ans>(st+i-2*ct)){ans = st+i-2*ct;}
+             else if(s[i]=='0'&&ans>(st+i-2*ct-1)){ans = st+i-2*ct-1;}
+             ct += (s[i]=='0');
          }
         return ans;
     }
