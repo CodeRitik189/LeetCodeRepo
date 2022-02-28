b52cb47814a2294da67e71b588449266846ff8ab
Ritik Tripathi <88445142+CodeRitik189@users.noreply.github.com>
2022-02-28 11:59:30 +0530

    Time: 35 ms (64.63%), Space: 11 MB (93.99%) - LeetHub

--- 926-flip-string-to-monotone-increasing/926-flip-string-to-monotone-increasing.cpp
+++ 926-flip-string-to-monotone-increasing/926-flip-string-to-monotone-increasing.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    //at any point '1' comapare with right 0's and left 1's
+    //at any point comapare with right 0's and left 1's
     int minFlipsMonoIncr(string s) {
          int st=0,ct=0,ans=s.size();
          for(int i=0;i<s.size();i++){st += (s[i]=='0');}
