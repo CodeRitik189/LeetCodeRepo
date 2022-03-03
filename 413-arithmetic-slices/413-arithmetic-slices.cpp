6e7be7b71c0d8502b1c397d1c7af0890280ee5cc
Ritik Tripathi <88445142+CodeRitik189@users.noreply.github.com>
2022-03-03 11:23:15 +0530

    Time: 6 ms (23.45%), Space: 9.7 MB (5.06%) - LeetHub

--- 413-arithmetic-slices/413-arithmetic-slices.cpp
+++ 413-arithmetic-slices/413-arithmetic-slices.cpp
@@ -3,15 +3,15 @@ public:
     int numberOfArithmeticSlices(vector<int>& nums) {
         int ans=0;
         if(nums.size()<3){return 0;}
-        vector<vector<int>>dp(nums.size(),vector<int>(2001,1));
+        vector<unordered_map<int,int>>dp(nums.size());
         for(int i=1;i<nums.size();i++){
-            dp[i][nums[i]-nums[i-1]+1000] = dp[i-1][nums[i]-nums[i-1]+1000]+1;
-            dp[i-1][nums[i]-nums[i-1]+1000] = 0;
+            dp[i][nums[i]-nums[i-1]] = dp[i-1][nums[i]-nums[i-1]]+1;
+            dp[i-1][nums[i]-nums[i-1]] = 0;
         }
         for(int i=0;i<dp.size();i++){
-            for(int j=0;j<2001;j++){
-                if(dp[i][j]>2){
-                    ans += (((dp[i][j]-2)*(dp[i][j]-1))/2);
+            for(auto j : dp[i]){
+                if(j.second>=2){
+                    ans += (((j.second)*(j.second-1))/2);
                 }
             }
         }
