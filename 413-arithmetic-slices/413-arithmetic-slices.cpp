28b109982ea1d933e2602d6fdfd9f71d160fe674
Ritik Tripathi <88445142+CodeRitik189@users.noreply.github.com>
2022-03-03 11:47:20 +0530

    Time: 5 ms (27.51%), Space: 7.3 MB (55.42%) - LeetHub

--- 413-arithmetic-slices/413-arithmetic-slices.cpp
+++ 413-arithmetic-slices/413-arithmetic-slices.cpp
@@ -1,9 +1,7 @@
 class Solution {
 public:
-    int numberOfArithmeticSlices(vector<int>& nums) {
-        int ans=0;
-        if(nums.size()<3){return 0;}
-        vector<unordered_map<int,int>>dp(nums.size());
+    // first approach was a o(N) space approach but new one is better 
+    /*vector<unordered_map<int,int>>dp(nums.size());
         for(int i=1;i<nums.size();i++){
             dp[i][nums[i]-nums[i-1]] = dp[i-1][nums[i]-nums[i-1]]+1;
             dp[i-1][nums[i]-nums[i-1]] = 0;
@@ -14,7 +12,21 @@ public:
                     ans += (((j.second)*(j.second-1))/2);
                 }
             }
+        }*/
+    int numberOfArithmeticSlices(vector<int>& nums) {
+        int ans=0;
+        if(nums.size()<3){return 0;}
+        int prev = nums[1]-nums[0],ct=2;
+        for(int i=2;i<nums.size();i++){
+            if(nums[i]-nums[i-1]==prev){
+                ct++;
+            }else{
+                ans += ((ct-1)*(ct-2))/2;
+                ct=2;
+                prev = nums[i]-nums[i-1];
+            }
         }
+        if(ct>2){ans += ((ct-1)*(ct-2))/2;}
        return ans;
     }
 };
