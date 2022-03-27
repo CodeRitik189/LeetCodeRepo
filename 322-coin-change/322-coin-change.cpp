1cc25d8cf28d71d1544a26e3c830ba80b9525ac1
Ritik Tripathi <88445142+CodeRitik189@users.noreply.github.com>
2022-03-27 23:59:37 +0530

    Time: 625 ms (5.01%), Space: 61.6 MB (7.69%) - LeetHub

---
+++ 322-coin-change/322-coin-change.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    unordered_map<int,int>dp;
+    int check(vector<int>& coins,int amt){
+        if(amt<0){return 1000000;}
+        if(amt==0){return 0;}
+        if(dp.find(amt)!=dp.end()){return dp[amt];}
+        int ans = 1000000;
+        for(int i=0;i<coins.size();i++){
+            ans = min(ans,check(coins,amt-coins[i]));
+        }
+        return dp[amt]=(ans==1000000?ans:1+ans);
+    }
+    int coinChange(vector<int>& coins, int amt) {
+        sort(coins.begin(),coins.end(),greater<int>());
+        int ans = check(coins,amt);
+        return ans==1000000?-1:ans;
+    }
+};
