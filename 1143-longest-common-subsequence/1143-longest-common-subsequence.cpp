9d35abbfa859599f5af03ed285b18659c0bce324
Ritik Tripathi <88445142+CodeRitik189@users.noreply.github.com>
2022-03-31 19:12:00 +0530

    Time: 634 ms (5.16%), Space: 82.3 MB (9.09%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -10,7 +10,17 @@ public:
     int longestCommonSubsequence(string text1, string text2) {
         // dp = vector<vector<int>>(text1.size(),vector<int>(text2.size(),-1));
         // return check(text1,text2,0,0);
-        vector<vector<int>>dp(text1.size()+1,vector<int>(text2.size()+1,0));
+        // vector<vector<int>>dp(text1.size()+1,vector<int>(text2.size()+1,0));
+        // int m = text1.size(),n = text2.size();
+        // for(int i=0;i<m;i++){dp[i][0]=0;}
+        // for(int i=0;i<n;i++){dp[0][i]=0;}
+        // for(int i=1;i<m+1;i++){
+        //     for(int j=1;j<n+1;j++){
+        //         dp[i][j] = (text1[i-1]==text2[j-1]?1+dp[i-1][j-1]:max(dp[i-1][j],dp[i][j-1]));
+        //     }
+        // }
+        // return dp[m][n];
+       unordered_map<int,unordered_map<int,int>>dp;
         int m = text1.size(),n = text2.size();
         for(int i=0;i<m;i++){dp[i][0]=0;}
         for(int i=0;i<n;i++){dp[0][i]=0;}
