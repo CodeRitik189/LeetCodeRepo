8d3d1742ff6ca824e8bccbd64332c772ef2cec63
Ritik Tripathi <88445142+CodeRitik189@users.noreply.github.com>
2022-03-31 19:14:09 +0530

    Time: 24 ms (61.70%), Space: 13.1 MB (17.47%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    /* Approach 1 recursion + memoisation */
     // vector<vector<int>>dp;
     // int check(string& t1,string& t2,int i,int j){
     // if(i==t1.size()||j==t2.size()){return 0;}
@@ -8,19 +9,10 @@ public:
     //                 max(check(t1,t2,i+1,j),check(t1,t2,i,j+1)));
     // }
     int longestCommonSubsequence(string text1, string text2) {
-        // dp = vector<vector<int>>(text1.size(),vector<int>(text2.size(),-1));
-        // return check(text1,text2,0,0);
-        // vector<vector<int>>dp(text1.size()+1,vector<int>(text2.size()+1,0));
-        // int m = text1.size(),n = text2.size();
-        // for(int i=0;i<m;i++){dp[i][0]=0;}
-        // for(int i=0;i<n;i++){dp[0][i]=0;}
-        // for(int i=1;i<m+1;i++){
-        //     for(int j=1;j<n+1;j++){
-        //         dp[i][j] = (text1[i-1]==text2[j-1]?1+dp[i-1][j-1]:max(dp[i-1][j],dp[i][j-1]));
-        //     }
-        // }
-        // return dp[m][n];
-       unordered_map<int,unordered_map<int,int>>dp;
+       // dp = vector<vector<int>>(text1.size(),vector<int>(text2.size(),-1));
+       // return check(text1,text2,0,0);
+        /* Approach 2 top down dp */
+        vector<vector<int>>dp(text1.size()+1,vector<int>(text2.size()+1,0));
         int m = text1.size(),n = text2.size();
         for(int i=0;i<m;i++){dp[i][0]=0;}
         for(int i=0;i<n;i++){dp[0][i]=0;}
@@ -30,5 +22,17 @@ public:
             }
         }
         return dp[m][n];
+        
+        /** Approach 3 map matrix **/
+       // unordered_map<int,unordered_map<int,int>>dp;
+       //  int m = text1.size(),n = text2.size();
+       //  for(int i=0;i<m;i++){dp[i][0]=0;}
+       //  for(int i=0;i<n;i++){dp[0][i]=0;}
+       //  for(int i=1;i<m+1;i++){
+       //      for(int j=1;j<n+1;j++){
+       //          dp[i][j] = (text1[i-1]==text2[j-1]?1+dp[i-1][j-1]:max(dp[i-1][j],dp[i][j-1]));
+       //      }
+       //  }
+       //  return dp[m][n];
     }
 };
