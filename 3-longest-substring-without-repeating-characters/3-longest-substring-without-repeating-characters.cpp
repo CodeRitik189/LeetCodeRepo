9af6394ad06b3cf6d398a290356909175ab32441
Ritik Tripathi <88445142+CodeRitik189@users.noreply.github.com>
2022-04-04 12:35:17 +0530

    Time: 14 ms (73.71%), Space: 8.3 MB (72.39%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -7,14 +7,11 @@ public:
             v[s[i]]++;
             if(v[s[i]]>1){
                 while((v[s[i]]>1&&j<i)){
-                 // if((s[i]>='a'&&s[i]<='z')||(s[i]>='A'&&s[i]<='Z')){
-                    v[s[j]]--;
-                 // }
+                  v[s[j]]--;
                   j++;
                 }
-            }else{
-                ans = max(ans,i-j+1);
             }
+            ans = max(ans,i-j+1);
             i++;
         }
         return ans;
