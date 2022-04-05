00c68321fe654ee5b032d07f6d08372818d41ebc
Ritik Tripathi <88445142+CodeRitik189@users.noreply.github.com>
2022-04-05 18:35:11 +0530

    Time: 15 ms (72.32%), Space: 8.3 MB (72.37%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -4,15 +4,14 @@ public:
         int i=0,j=0,ans=0;
         vector<int>v(256,0);
         while(i<s.size()){
-            v[s[i]]++;
-            if(v[s[i]]>1){
-                while((v[s[i]]>1&&j<i)){
-                  v[s[j]]--;
-                  j++;
-                }
+            if(v[s[i]]>0){
+                v[s[j]]--;
+                j++; 
+            }else{
+                v[s[i]]++;
+                ans = max(ans,i-j+1);
+                i++;
             }
-            ans = max(ans,i-j+1);
-            i++;
         }
         return ans;
     }
