ec68be6edf2dc8d3e36c39e9612d31b9c1763f37
Ritik Tripathi <88445142+CodeRitik189@users.noreply.github.com>
2022-04-06 20:14:40 +0530

    Time: 11 ms (24.14%), Space: 15.2 MB (25.61%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.cpp
+++ 22-generate-parentheses/22-generate-parentheses.cpp
@@ -3,7 +3,7 @@ public:
     int sz;
     vector<string>vs;
     void check(string s,int ct){
-        if(ct<0){return;}
+        if(ct<0||ct>2*sz-s.size()){return;}
         if(s.size()==2*sz){
           if(ct==0){
               vs.push_back(s);
