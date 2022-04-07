eb3a898d0fab6be627aadde76e8a30d1552d7bfc
Ritik Tripathi <88445142+CodeRitik189@users.noreply.github.com>
2022-04-07 22:33:01 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (89.82%) - LeetHub

--- 394-decode-string/394-decode-string.cpp
+++ 394-decode-string/394-decode-string.cpp
@@ -7,17 +7,22 @@ public:
         int i=st;
         string ans="";
         while(i<=endi){
+           //add starting characters to ans
            while(i<=endi&&(s[i]>='a'&&s[i]<='z')){
                 ans += s[i];
                 i++;
            }
            int num = 0;
+           //if there is a number calculate this
            while(i<=endi&&(s[i]>='0'&&s[i]<='9')){
               num = num*10 + (s[i]-'0');
               i++;
            }
+           //now its confirmed that s[i] is '['
            int j = i+1;
            int ct = 1;
+           
+           //check the closing bracket at j for corresponding i
            while(j<=endi){
                if(s[j]=='['){
                    ct++;
@@ -29,11 +34,14 @@ public:
                }
                j++;
            }
+           //find ans inside this bracket using recursion and multiply acc to number
            string ansprev = check(s,i+1,j-1);
            while(num--)
              ans += ansprev;
+            
+           //if there further string is left give value j+1 to i for next iteration
            i = j+1;
-        }
+       }
         return ans;
     }
     string decodeString(string s){
