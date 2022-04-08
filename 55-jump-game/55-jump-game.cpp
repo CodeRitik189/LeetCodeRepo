6b81447ad81b758961ec67ea7d5b8628800bcf4a
Ritik Tripathi <88445142+CodeRitik189@users.noreply.github.com>
2022-04-08 23:36:26 +0530

    Time: 105 ms (41.46%), Space: 48.4 MB (36.28%) - LeetHub

--- 55-jump-game/55-jump-game.cpp
+++ 55-jump-game/55-jump-game.cpp
@@ -3,7 +3,7 @@ public:
     bool canJump(vector<int>& nums) {
         int mx = 0;
         for(int i = 0; i<nums.size()-1; i++){
-            if(nums[i]==0&&i+nums[i]>=mx){
+            if(nums[i]==0&&i>=mx){
                 return false;
             }
             mx = max(mx,i+nums[i]);
