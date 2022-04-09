46c447f64a44b8f84a3615173af011221966e3f4
Ritik Tripathi <88445142+CodeRitik189@users.noreply.github.com>
2022-04-09 11:21:47 +0530

    Added solution - LeetHub

--- Faulty wiring and Bulbs - GFG/faulty-wiring-and-bulbs.cpp
+++ Faulty wiring and Bulbs - GFG/faulty-wiring-and-bulbs.cpp
@@ -7,19 +7,8 @@ using namespace std;
 class Solution
 {
   public:
-    // int check(int* a , int idx ,int ct, int n){
-    //     if(idx==n){
-    //         return 0;
-    //     }
-    //     if(a[idx]==ct){
-    //         return check(a, idx+1, ct, n);
-    //     }else{
-    //         return 1+check(a,idx+1,1-ct,n);
-    //     }
-    // }
     int countFlips(int a[],int n)
     {
-        //return check(a, 0 ,1 ,n );//code here.
         int ct = 1,ans = 0;
         for(int i = 0 ; i<n ; i++){
             if(ct!=a[i]){
