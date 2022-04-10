fbbc338f5d8c53cd61a8eaa3c10e31795c918d95
Ritik Tripathi <88445142+CodeRitik189@users.noreply.github.com>
2022-04-10 09:43:10 +0530

    Time: 9 ms (10.78%), Space: 9.1 MB (6.04%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -1,32 +1,20 @@
 class Solution {
 public:
-  int counti( string s , int i , int* dp ){
-      if(s.size()==i){
-         return 1;
-      }
-       if(i==(s.size()-1)&&s[ i ]!='0'){
-          dp[ i ] = 1;
-           return 1;
-       }else if(s[ i ]=='0'){
-           dp[ i ]=0;
-           return 0;
-         }
-         if( dp[ i ]!=-1){
-           return dp[ i ] ;
-         }
-           int ans = 0;
-           if(s[i]=='1'||(s[i]=='2'&&s[i+1]<='6')){
-               ans += counti(s,i+2 , dp);
-           }
-           ans += counti(s,i+1 , dp);
-           dp[ i ] = ans;
-           return ans;
-   }
-    int numDecodings(string s) {
-        int* dp = new int [ s.size( ) ];
-        for( int i = 0 ; i< s.size( ); i++){
-             dp[ i ]= -1;
+    vector<vector<int>>dp;
+    int find(string& s, int idx, int num){
+        if(num>26){
+            return 0;
         }
-        return counti(s ,0 , dp);
+        if(idx==s.size()){
+            return 1;
+        }
+        if(dp[idx][num]!=-1){
+            return dp[idx][num];
+        }
+ return dp[idx][num] = find(s,idx+1,num*10+(s[idx]-'0'))+(s[idx]!='0'?find(s,idx+1,(s[idx]-'0')):0);
+    }
+    int numDecodings(string s) {
+        dp = vector<vector<int>>(s.size(),vector<int>(100,-1));
+        return s[0]!='0'?find(s,1,s[0]-'0'):0;
     }
 };
