3a3b3f308f482d6da9f74c219119f4661ca44014
Ritik Tripathi <88445142+CodeRitik189@users.noreply.github.com>
2022-04-19 12:13:53 +0530

    Time: 16 ms (26.48%), Space: 7.7 MB (34.48%) - LeetHub

--- 142-linked-list-cycle-ii/142-linked-list-cycle-ii.cpp
+++ 142-linked-list-cycle-ii/142-linked-list-cycle-ii.cpp
@@ -8,12 +8,14 @@
  */
 class Solution {
 public:
+    //as u cannot modify the linkedlist
     ListNode *detectCycle(ListNode *head) {
         if(!head||!head->next){return NULL;}
         bool fl = false;
         ListNode* t1 = head;
         ListNode* t2 = head->next;
         
+        // check whether cycle is present or not
         while(t2&&t2->next){
             if(t2==t1){
               fl = true;
@@ -25,6 +27,7 @@ public:
         if(!fl)
         return NULL; 
         
+    // if cycle exists count number of nodes in the cycle
         t1 = t1->next;
         int ct = 1;
         while(t1!=t2){
@@ -35,9 +38,12 @@ public:
         t1 = head;
         t2 = head;
         
+       // now start with head and create a window of cycle nodes
         while(ct--){
            t2 = t2 -> next; 
         }
+        
+    // slide the window until when both the pointers are pointing to the first node of the cycle
         while(t1!=t2){
             t1 = t1->next;
             t2 = t2->next;
