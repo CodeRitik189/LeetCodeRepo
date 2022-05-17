b8c845ddb3409b157d89dd2c14e3b316f4c622b7
Ritik Tripathi <88445142+CodeRitik189@users.noreply.github.com>
2022-05-17 11:33:40 +0530

    Time: 26 ms (72.14%), Space: 8.2 MB (97.23%) - LeetHub

--- 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.cpp
+++ 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.cpp
@@ -1,28 +1,6 @@
 class Solution {
 public:
     int ct=0,mx1=-34;
-//     void count(vector<int>& nums, int mask,int val,vector<bool>& check,int ct){
-//             if(check[mask]){
-//               return;
-//             }else{
-//                 check[mask] = true;
-//             }
-          
-//              if(ct!=0){
-//               if(val>mx){
-//                  mx = val;
-//                  mx1=1;
-//               }else if(val==mx){
-//                  mx1++;
-//               }
-//              }
-//         for(int i=0;i<nums.size();i++){
-//             if(!(mask&(1<<i))){
-//                 count(nums,mask|(1<<i),val|nums[i],check,ct+1);
-//             }
-//         }
-//         return;
-//     }
     void count(vector<int>& nums,int val,int idx){
         if(idx==nums.size()){
             if(val>mx1){
@@ -38,7 +16,6 @@ public:
         return;
     }
     int countMaxOrSubsets(vector<int>& nums) {
-         //vector<bool>check(pow(2,nums.size()+1),false);
          count(nums,0,0);
          return ct;
     }
