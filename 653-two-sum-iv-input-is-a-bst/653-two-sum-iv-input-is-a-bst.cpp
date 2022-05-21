2ae653f1c87af5756a7b9cd6a2bbb5a10ea1f81a
Ritik Tripathi <88445142+CodeRitik189@users.noreply.github.com>
2022-05-21 17:44:27 +0530

    Time: 42 ms (82.49%), Space: 37.6 MB (42.84%) - LeetHub

--- 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
@@ -11,25 +11,73 @@
  */
 class Solution {
 public:
-    unordered_map<int,int>um;
-    int i=0;
-    void preorder(TreeNode* root){
-        if(!root){return;}
-        um[root->val]= i;
-        i++;
-        preorder(root->left);
-        preorder(root->right);
+//     unordered_map<int,int>um;
+//     int i=0;
+//     void preorder(TreeNode* root){
+//         if(!root){return;}
+//         um[root->val]= i;
+//         i++;
+//         preorder(root->left);
+//         preorder(root->right);
+//     }
+    
+//     bool check(TreeNode* root,int k){
+//         if(!root){return false;}
+//         if(um.find(k-root->val)!=um.end()&&um[k-root->val]!=um[root->val]){
+//            return true;
+//         }
+//         return check(root->left,k)||check(root->right,k);
+//     }
+    stack<TreeNode*>prev;
+    stack<TreeNode*>nxt;
+    
+    void create(TreeNode* root){
+        pushleft(root);
+        pushright(root);
     }
     
-    bool check(TreeNode* root,int k){
-        if(!root){return false;}
-        if(um.find(k-root->val)!=um.end()&&um[k-root->val]!=um[root->val]){
-           return true;
-        }
-        return check(root->left,k)||check(root->right,k);
+    void pushleft(TreeNode* root){
+        if(!root){return ;}
+        nxt.push(root);
+        pushleft(root->left);
     }
+    void pushright(TreeNode* root){
+        if(!root){return ;}
+        prev.push(root);
+        pushright(root->right);
+    }
+    
+    TreeNode* next(){
+        TreeNode* nx = nxt.top();
+        nxt.pop();
+        pushleft(nx->right);
+        return nx;
+    }
+    
+    TreeNode* previous(){
+        TreeNode* pr = prev.top();
+        prev.pop();
+        pushright(pr->left);
+        return pr;
+    }
+    
     bool findTarget(TreeNode* root, int k) {
-        preorder(root);
-        return check(root,k);
+        //preorder(root);
+        //return check(root,k);
+        create(root);
+        
+        TreeNode* i = next();
+        TreeNode* j = previous();
+        
+        while(i->val<j->val){
+            if(i->val+j->val == k){
+                return true;
+            }else if(i->val+j->val<k){
+                i = next();
+            }else{
+                j = previous();
+            }
+        }
+        return false;
     }
 };
