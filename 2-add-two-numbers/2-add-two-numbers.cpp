f92e393f3e35265301293049dfde73eda6dc87aa
Ritik Tripathi <88445142+CodeRitik189@users.noreply.github.com>
2022-05-22 10:30:51 +0530

    Time: 39 ms (74.94%), Space: 71.3 MB (82.03%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.cpp
+++ 2-add-two-numbers/2-add-two-numbers.cpp
@@ -11,41 +11,52 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        int val=0,carry=0;
         ListNode* head=NULL;
-        ListNode* temp=NULL;
+        ListNode* temp = head;
+        int carry=0,vl=0;
         while(l1&&l2){
-           val = (l1->val+l2->val+carry)%10;
-           carry = (l1->val+l2->val+carry)/10;
-           ListNode* t = new ListNode(val);
-           if(!head){
-               head = t;
-               temp = t;
-           }
-           else{temp->next = t;temp = temp->next;}
-           l1 = l1->next;
-           l2 = l2->next;
+            vl = (carry+l1->val+l2->val)%10;
+            carry = (carry+l1->val+l2->val)/10;
+            ListNode* nxt = new ListNode(vl);
+            if(!head){
+              head = nxt;  
+              temp = nxt;
+            }else{
+               temp->next =  nxt;
+               temp = temp->next;
+            }
+            l1 = l1->next;
+            l2 = l2->next;
         }
         while(l1){
-           val = (l1->val+carry)%10;
-           carry = (l1->val+carry)/10;
-           ListNode* t = new ListNode(val);
-           temp->next = t;
-           temp = temp->next;
-           l1 = l1->next;
+            vl = (carry+l1->val)%10;
+            carry = (carry+l1->val)/10;
+            ListNode* nxt = new ListNode(vl);
+            if(!head){
+              head = nxt;  
+              temp = nxt;
+            }else{
+               temp->next =  nxt;
+               temp = temp->next;
+            } 
+            l1 = l1->next;
         }
         while(l2){
-           val = (l2->val+carry)%10;
-           carry = (l2->val+carry)/10;
-           ListNode* t = new ListNode(val);
-           temp->next = t;
-           temp = temp->next;
-           l2 = l2->next;
+            vl = (carry+l2->val)%10;
+            carry = (carry+l2->val)/10;
+            ListNode* nxt = new ListNode(vl);
+            if(!head){
+              head = nxt;  
+              temp = nxt;
+            }else{
+               temp->next =  nxt;
+               temp = temp->next;
+            }
+            l2 = l2->next;
         }
-        if(carry!=0){
-           ListNode* t = new ListNode(carry);
-           temp->next = t;
-           temp = temp->next; 
+        if(carry>0){
+           ListNode* nxt = new ListNode(carry);
+           temp->next =  nxt;
         }
         return head;
     }
