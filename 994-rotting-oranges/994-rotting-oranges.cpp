9ad562b302d5b7996d8c227e2c004fb731cb52c3
Ritik Tripathi <88445142+CodeRitik189@users.noreply.github.com>
2022-05-28 17:11:15 +0530

    Time: 11 ms (52.81%), Space: 13.2 MB (49.92%) - LeetHub

---
+++ 994-rotting-oranges/994-rotting-oranges.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+    void insert(queue<pair<int,int>>& qp,vector<vector<int>>& grid, pair<int,int>& p ){
+        int i = p.first, j = p.second;
+        
+        if(i>0 && grid[i-1][j] == 1){
+            grid[i-1][j] = 2;
+            qp.push({i-1,j}); 
+        }
+        if(i<grid.size()-1 && grid[i+1][j] == 1){
+             grid[i+1][j] = 2;
+            qp.push({i+1,j}); 
+        }
+        if(j>0 && grid[i][j-1] == 1){
+             grid[i][j-1] = 2;
+            qp.push({i,j-1}); 
+        }
+        if(j<grid[0].size()-1 && grid[i][j+1] == 1){
+             grid[i][j+1] = 2;
+            qp.push({i,j+1}); 
+        }
+    }
+    int orangesRotting(vector<vector<int>>& grid) {
+        
+        queue<pair<int,int>>qp;
+        
+        for(int i=0;i<grid.size();i++){
+            for(int j=0;j<grid[0].size();j++){
+                if(grid[i][j]==2){
+                    qp.push({i,j});
+                }
+            }
+        }
+        
+        int ct = 0 ;
+        while(!qp.empty()){
+           int k = qp.size();
+           while(k--){
+              pair<int,int>p = qp.front();
+              qp.pop();
+              insert(qp,grid,p);
+           }
+            ct++;
+        }
+        
+        for(int i=0;i<grid.size();i++){
+            for(int j=0;j<grid[0].size();j++){
+                if(grid[i][j]==1){
+                    return -1;
+                }
+            }
+        }
+        
+        return max(0,ct-1);
+    }
+};
