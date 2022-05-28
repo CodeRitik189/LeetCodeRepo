99ae0d48d761535d2af3ca637479d71f3a3a3d70
Ritik Tripathi <88445142+CodeRitik189@users.noreply.github.com>
2022-05-28 18:52:09 +0530

    Time: 32 ms (10.20%), Space: 9.7 MB (17.41%) - LeetHub

--- 12-integer-to-roman/12-integer-to-roman.cpp
+++ 12-integer-to-roman/12-integer-to-roman.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     string intToRoman(int num) {
-        unordered_map<int , char>um;
+        vector<char>um(7);
         um[0] = 'I'; um[1] = 'V'; um[2] = 'X';
         um[3] = 'L'; um[4] = 'C'; um[5] = 'D';
         um[6] = 'M';
         
-        unordered_map<int,vector<int>>um2;
+        vector<vector<int>>um2(10);
         um2[0] = {}; um2[1] = {0}; um2[2] = {0,0}; um2[3] = {0,0,0}; um2[4] = {1,0}; 
         um2[5] = {1};um2[6] = {0,1};um2[7] = {0,0,1};um2[8] = {0,0,0,1};um2[9] = {2,0};
         
