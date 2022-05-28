73b5110fc77a73aa9ed14057da6e97d082489b20
Ritik Tripathi <88445142+CodeRitik189@users.noreply.github.com>
2022-05-28 17:21:54 +0530

    Time: 25 ms (6.29%), Space: 13.2 MB (49.92%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.cpp
+++ 994-rotting-oranges/994-rotting-oranges.cpp
@@ -45,14 +45,6 @@ public:
             ct++;
         }
         
-        // for(int i=0;i<grid.size();i++){
-        //     for(int j=0;j<grid[0].size();j++){
-        //         if(grid[i][j]==1){
-        //             return -1;
-        //         }
-        //     }
-        // }
-        
         return crt==0?max(0,ct-1):-1;
     }
 };
