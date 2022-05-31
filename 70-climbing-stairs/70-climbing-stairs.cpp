c4a100b95ff2792b937da4b25cea4ccf8b503c61
Ritik Tripathi <88445142+CodeRitik189@users.noreply.github.com>
2022-05-31 10:46:13 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (56.56%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,17 +1,29 @@
 class Solution {
 public:
-    vector<int>v;
-    int climb(int n){
-        if(n<=0){
-            return n==0;
-        }
-        if(v[n]!=-1){
-            return v[n];
-        }
-        return v[n] = climb(n-1)+climb(n-2);
-    }
+    // vector<int>v;
+    // int climb(int n){
+    //     if(n<=0){
+    //         return n==0;
+    //     }
+    //     if(v[n]!=-1){
+    //         return v[n];
+    //     }
+    //     return v[n] = climb(n-1)+climb(n-2);
+    // }
     int climbStairs(int n) {
-        v = vector<int>(n+1,-1);
-        return climb(n);
+        // v = vector<int>(n+1,-1);
+        // return climb(n);
+        if(n<=1){
+            return n;
+        }
+        
+        int pr1 = 1,pr2 = 1;
+        
+        for(int i=2;i<=n;i++){
+            int t = pr1;
+            pr1 = pr2;
+            pr2 += t;
+        }
+        return pr2;
     }
 };
