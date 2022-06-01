fb588e6e66e131e78bc3ce322c009efe46dce1f8
Ritik Tripathi <88445142+CodeRitik189@users.noreply.github.com>
2022-06-01 18:45:20 +0530

    Time: 223 ms (14.46%), Space: 14.6 MB (34.63%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -7,12 +7,24 @@ public:
         int ans = 1000000;
         for(int i=0;i<coins.size();i++){
             if(amt>=coins[i])
-            ans = min(ans,check(coins,amt-coins[i]));
+             ans = min(ans,check(coins,amt-coins[i]));
         }
         return dp[amt]=(ans==1000000?ans:1+ans);
     }
     int coinChange(vector<int>& coins, int amt) {
-        int ans = check(coins,amt);
-        return ans==1000000?-1:ans;
+        // int ans = check(coins,amt);
+        // return ans==1000000?-1:ans;
+        int n = coins.size();
+        vector<int>dp(amt+1,INT_MAX);
+        
+        dp[0] = 0;
+        
+        for(int i = 1; i<= amt ;i++){
+            for(int j = 0; j< n; j++){
+               if(i>=coins[j] && dp[i-coins[j]]!=INT_MAX)
+                 dp[i] = min(dp[i],1+dp[i-coins[j]]);
+            }
+        }
+        return dp[amt]==INT_MAX ? -1 : dp[amt];
     }
 };
