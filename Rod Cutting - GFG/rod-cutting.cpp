21583aabf5f7bb6e1d86cf9f25b899c2f67655bc
Ritik Tripathi <88445142+CodeRitik189@users.noreply.github.com>
2022-06-02 17:38:29 +0530

    Added solution - LeetHub

--- Rod Cutting - GFG/rod-cutting.cpp
+++ Rod Cutting - GFG/rod-cutting.cpp
@@ -23,8 +23,15 @@ class Solution{
     }
     int cutRod(int price[], int n) {
         N = n;
-        dp = vector<int>(n+1,-1);
-        return check(price,n);
+        dp = vector<int>(n+1,0);
+        
+       // return check(price,n);
+       for(int i=1; i<= n ;i++){
+         for(int j = 1; j<=i ; j++){
+             dp[i] = max(dp[i],price[j-1]+dp[i-j]);
+         }
+       }
+        return dp[n];
     }
 };
 
