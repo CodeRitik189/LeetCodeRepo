5126a9286e6d20c554c52df69b940737647212d1
Ritik Tripathi <88445142+CodeRitik189@users.noreply.github.com>
2022-06-03 15:52:25 +0530

    Time: 53 ms (71.63%), Space: 27.6 MB (22.43%) - LeetHub

--- 44-wildcard-matching/44-wildcard-matching.cpp
+++ 44-wildcard-matching/44-wildcard-matching.cpp
@@ -2,18 +2,15 @@ class Solution {
 public:
     vector<vector<int>>dp;
     bool check(string& s, string& t, int i , int j){
-        if(j==t.size()){
-            return i==s.size();
-        }
-        if(i==s.size()){
-            return (t[j]=='*'&&check(s,t,i,j+1));
-        }
+        if(j==t.size()){   return i==s.size();    }
+        if(i==s.size()){   return (t[j]=='*'&&check(s,t,i,j+1));  }
         
         if(dp[i][j]!=-1){ return dp[i][j]; }
         
         if(s[i]==t[j]){
-            dp[i][j] = check(s,t,i+1,j+1);
+            dp[i][j] = check(s,t,i+1,j+1);  
         }else{
+            
             if(t[j]=='?'){
                  dp[i][j] = check(s,t,i+1,j+1);
             }else if(t[j]=='*'){
@@ -21,6 +18,7 @@ public:
             }else{
                 dp[i][j] = false;
             }
+            
         }
         return dp[i][j];
     }
