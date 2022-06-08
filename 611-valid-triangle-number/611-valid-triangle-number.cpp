833275fb6534b142214fc9281b87ee1967fa1008
Ritik Tripathi <88445142+CodeRitik189@users.noreply.github.com>
2022-06-08 17:19:08 +0530

    Time: 153 ms (54.22%), Space: 13 MB (52.99%) - LeetHub

--- 611-valid-triangle-number/611-valid-triangle-number.cpp
+++ 611-valid-triangle-number/611-valid-triangle-number.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
-    int BS(vector<int>& nums,int st,int endi,int sum){
-        if(st>endi){return -1;}
-        int mid = (st+endi)/2;
-        if(nums[mid]<sum){return max(mid,BS(nums,mid+1,endi,sum));}
-        else{return BS(nums,st,mid-1,sum);}
-    }
     int triangleNumber(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        int ct=0;
-        for(int i=0;i<nums.size();i++){
-            for(int j=i+1;j<nums.size()-1;j++){
-                int idx = BS(nums,j+1,nums.size()-1,nums[i]+nums[j]);
-                if(idx!=-1)
-                ct += (idx-j);
+        sort(nums.begin(), nums.end());
+        int n = nums.size(), ans = 0;
+        
+        for(int i=n-1; i>=2; i--){
+            int k = i-1 , j = 0;
+            
+            while(j<k){
+                if(nums[j]+nums[k]>nums[i]){
+                    ans+=(k-j);
+                    k--;
+                }else{
+                    j++; 
+                }
             }
         }
-        return ct;
+        return ans;
     }
 };
