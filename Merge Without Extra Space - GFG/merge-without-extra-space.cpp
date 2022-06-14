c01b318f506b13e5fe3c1aeb9f37162953b8cb38
Ritik Tripathi <88445142+CodeRitik189@users.noreply.github.com>
2022-06-14 23:35:55 +0530

    Added solution - LeetHub

--- Merge Without Extra Space - GFG/merge-without-extra-space.cpp
+++ Merge Without Extra Space - GFG/merge-without-extra-space.cpp
@@ -1,58 +1,80 @@
 // { Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
+#include <bits/stdc++.h> 
+using namespace std; 
+
 
  // } Driver Code Ends
-//User function template for C++
 class Solution{
-public:
-   // use gap algo
-   
-	void merge(int ar1[], int ar2[], int m, int n) {
-           int gap = ((m+n)%2==0 ? (m+n) : (m+n+1))/2;
-           
-           while( gap != 0){
-               int i = 0, j = i+gap;
-               while( j < m+n ){
+    public:
+        //Function to merge the arrays.
+        void merge(long long arr1[], long long arr2[], int m, int n) 
+        { 
+            // code here 
+            int gap = (n+m+1)/2;
+            
+            while(gap != 0){
+                int i = 0, j = i+gap;
+                
+                while(j<m+n){
+                    if(i<m && j<m){
+                        if(arr1[i]>arr1[j]){
+                            swap(arr1[i],arr1[j]);
+                        }
+                    }else if(i >= m){
+                        if(arr2[i-m]>arr2[j-m]){
+                            swap(arr2[i-m],arr2[j-m]);
+                        }
+                    }else{
+                        if(arr1[i]>arr2[j-m]){
+                            swap(arr1[i],arr2[j-m]);
+                        }
+                    }
+                    i++;
+                    j++;
+                }
+                if(gap == 1)
+                   gap = 0 ;
+                 else
+                   gap = gap/2 + gap%2;
                    
-                   if(j >= m && i>= m){
-                       if(ar2[i-m]>ar2[j-m]){  swap(ar2[i-m],ar2[j-m]);  }
-                   }else if( j >= m){
-                        if(ar1[i]>ar2[j-m]){  swap(ar1[i],ar2[j-m]);  }
-                   }else{
-                       if(ar1[i]>ar1[j]){  swap(ar1[i],ar1[j]);  }
-                   }
-                   i++;
-                   j++;
-               }
-               gap = (gap%2== 0||gap==1) ? gap/2 : (gap+1)/2;
-           }
-	}
+            }
+        } 
 };
 
 // { Driver Code Starts.
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int n, m, i;
-        cin >> n >> m;
-        int arr1[n], arr2[m];
-        for (i = 0; i < n; i++) {
-            cin >> arr1[i];
-        }
-        for (i = 0; i < m; i++) {
-            cin >> arr2[i];
-        }
-        Solution ob;
-        ob.merge(arr1, arr2, n, m);
-        for (i = 0; i < n; i++) {
-            cout << arr1[i] << " ";
-        }
-        for (i = 0; i < m; i++) {
-            cout << arr2[i] << " ";
-        }
-        cout << "\n";
-    }
-    return 0;
-}  // } Driver Code Ends
+
+int main() 
+{ 
+	
+	int T;
+	cin >> T;
+	
+	while(T--){
+	    int n, m;
+	    cin >> n >> m;
+	    
+	    long long arr1[n], arr2[m];
+	    
+	    for(int i = 0;i<n;i++){
+	        cin >> arr1[i];
+	    }
+	    
+	    for(int i = 0;i<m;i++){
+	        cin >> arr2[i];
+	    }
+	    Solution ob;
+	    ob.merge(arr1, arr2, n, m); 
+
+        for (int i = 0; i < n; i++) 
+            cout<<arr1[i]<<" "; 
+        
+       
+	    for (int i = 0; i < m; i++) 
+		    cout<<arr2[i]<<" "; 
+	    
+	    cout<<endl;
+	}
+
+	return 0; 
+} 
+  // } Driver Code Ends
