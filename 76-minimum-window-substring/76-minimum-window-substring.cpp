5e713dd907fa0ac9bb3a37be6310a12c9bd2f8ed
Ritik Tripathi <88445142+CodeRitik189@users.noreply.github.com>
2022-06-24 19:31:32 +0530

    Time: 74 ms (20.91%), Space: 7.7 MB (69.40%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.cpp
+++ 76-minimum-window-substring/76-minimum-window-substring.cpp
@@ -4,15 +4,15 @@ public:
         
  int i = 1, j = 0, n1 = s.size(), n2 = t.size(), ans = s.size()+1, idx = -1;
         
-        vector<int>v1(256,0);
-        vector<int>v2(256,0);
+        vector<int>v1(58,0);
+        vector<int>v2(58,0);
         
-        for(int k = 0; k<n2; k++){ v1[t[k]]++; }      
-        v2[s[0]]++;
+        for(int k = 0; k<n2; k++){ v1[t[k]-'A']++; }      
+        v2[s[0]-'A']++;
         
         while(i<=n1 && j<n1){
             bool fl = true;
-            for(int k = 0; k<256; k++){
+            for(int k = 0; k<58; k++){
                 if(v2[k]-v1[k] < 0){
                     fl = false;
                     break;
@@ -23,10 +23,10 @@ public:
                   ans = i-j;
                   idx = j;
                 }
-                v2[s[j]]--;
+                v2[s[j]-'A']--;
                 j++;
             }else{
-                if(i<n1)v2[s[i]]++;
+                if(i<n1)v2[s[i]-'A']++;
                 i++;
             }
         }
