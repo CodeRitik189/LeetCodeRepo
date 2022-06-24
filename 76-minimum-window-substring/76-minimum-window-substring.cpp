598454f708320a4132a02a952c6b1dbb497f7104
Ritik Tripathi <88445142+CodeRitik189@users.noreply.github.com>
2022-06-24 19:26:26 +0530

    Time: 672 ms (5.03%), Space: 8.3 MB (32.38%) - LeetHub

---
+++ 76-minimum-window-substring/76-minimum-window-substring.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        
+ int i = 1, j = 0, n1 = s.size(), n2 = t.size(), ans = s.size()+1, idx = -1;
+        
+        vector<int>v1(256,0);
+        vector<int>v2(256,0);
+        
+        for(int k = 0; k<n2; k++){ v1[t[k]]++; }      
+        v2[s[0]]++;
+        
+        while(i<=n1 && j<n1){
+            bool fl = true;
+            for(int k = 0; k<256; k++){
+                if(v2[k]-v1[k] < 0){
+                    fl = false;
+                    break;
+                }
+            }
+            if(fl){
+                if(ans> i-j){
+                  ans = i-j;
+                  idx = j;
+                }
+                v2[s[j]]--;
+                j++;
+            }else{
+                if(i<n1)v2[s[i]]++;
+                i++;
+            }
+        }
+        return idx == -1 ? "" : s.substr(idx,ans);
+    }
+};
