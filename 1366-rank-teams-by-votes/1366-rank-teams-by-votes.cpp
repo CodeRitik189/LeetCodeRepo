8ea1ef7137b2c6080177423382507811d80973b3
Ritik Tripathi <88445142+CodeRitik189@users.noreply.github.com>
2022-06-28 18:43:30 +0530

    Time: 37 ms (20.99%), Space: 10.6 MB (76.19%) - LeetHub

--- 1366-rank-teams-by-votes/1366-rank-teams-by-votes.cpp
+++ 1366-rank-teams-by-votes/1366-rank-teams-by-votes.cpp
@@ -1,23 +1,19 @@
 class Solution {
 public:
-    static bool cmp(vector<int> v1, vector<int> v2){
-        return v1>v2;
-    }
+
     string rankTeams(vector<string>& votes) {
         vector<vector<int>>v(26,vector<int>(27,0));
         
-        for(int i = 0; i<26; i++){
-           v[i][26] = 25-i;
-        }
+        for(int i = 0; i<26; i++){  v[i][26] = 25-i; }
         for(int i = 0; i<votes.size(); i++){
             for(int j = 0; j<votes[i].size(); j++)
                v[votes[i][j]-'A'][j]++;
         }
         
-        sort(v.begin(),v.end(),cmp);
+        sort(v.begin(),v.end());
         string ans = "";
         
-        for(int i = 0 ; i<26; i++){
+        for(int i = 25 ; i>= 0; i--){
             bool fl = false;
             for(int j = 0; j<26; j++){
                 if(v[i][j] >0){
@@ -25,11 +21,8 @@ public:
                     break;
                 }
             }
-            if(fl){
-                ans += ((25-v[i][26])+'A');
-            }else{
-                break;
-            }
+            if(fl)ans += ((25-v[i][26])+'A');
+            else break; 
         }
         return ans;
     }
