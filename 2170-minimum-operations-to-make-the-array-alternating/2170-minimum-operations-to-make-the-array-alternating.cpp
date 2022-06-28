ebf85b18c7c762e0a8197e5b91668bf2bd9c9e11
Ritik Tripathi <88445142+CodeRitik189@users.noreply.github.com>
2022-06-28 16:23:42 +0530

    Time: 371 ms (87.21%), Space: 140.5 MB (63.68%) - LeetHub

--- 2170-minimum-operations-to-make-the-array-alternating/2170-minimum-operations-to-make-the-array-alternating.cpp
+++ 2170-minimum-operations-to-make-the-array-alternating/2170-minimum-operations-to-make-the-array-alternating.cpp
@@ -1,42 +1,39 @@
 class Solution {
 public:
-    static bool cmp(vector<int>& p1, vector<int>& p2){
-        return p1[1] > p2[1];
-    }
-    void insert(unordered_map<int,int>& um,vector<vector<int>>& v){
-        for(auto x:um){
-            v.push_back({x.first,x.second});
+    vector<int> check(unordered_map<int,int>& um){
+        int I = INT_MIN, Is, II = INT_MIN ;
+        
+        for(auto x: um){
+            if(x.second > I){
+                II = I;
+                I = x.second;
+                Is = x.first;
+            }else if(x.second > II){
+                II = x.second;
+            }
         }
+        return {Is,I,max(II,0)};
     }
     int minimumOperations(vector<int>& nums) {
         if(nums.size()==1){return 0;}
-        int ec = 0, oc = 0;
+        
         unordered_map<int, int> m1;
         unordered_map<int, int> m2;
         
         for(int i = 0; i<nums.size(); i++){
             if(i%2==0){
-                ec++;
                 m1[nums[i]]++ ;
             }else{
-               oc++;
                m2[nums[i]]++;
             }
         }
-        vector<vector<int>>v1;
-        vector<vector<int>>v2;
-        
-        insert(m1,v1);
-        insert(m2,v2);
-        
-        sort(v1.begin(),v1.end(),cmp);
-        sort(v2.begin(),v2.end(),cmp);
-        
-        if(v1[0][0]==v2[0][0]){
-     return nums.size()-max((v1[0][1]+(v2.size()>1?v2[1][1]:0))
-                            ,(v2[0][1]+(v1.size()>1?v1[1][1]:0)));
+       vector<int>v1 = check(m1);
+       vector<int>v2 = check(m2);
+
+        if(v1[0]==v2[0]){
+           return nums.size()-max(v1[1]+v2[2],v2[1]+v1[2]);
         }else{
-            return nums.size() - (v1[0][1]+v2[0][1]); 
+           return nums.size()-(v1[1]+v2[1]); 
         }
     }
 };
