3786e080e9fc8df950034b0fd31f51d28c22e7b6
Ritik Tripathi <88445142+CodeRitik189@users.noreply.github.com>
2022-06-28 16:33:12 +0530

    Time: 199 ms (94.74%), Space: 121 MB (93.80%) - LeetHub

--- 2216-minimum-deletions-to-make-array-beautiful/2216-minimum-deletions-to-make-array-beautiful.cpp
+++ 2216-minimum-deletions-to-make-array-beautiful/2216-minimum-deletions-to-make-array-beautiful.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
     int minDeletion(vector<int>& nums) {
-        int prev = 0, ct = 0, oct = 1;
+        int  ct = 0, oct = 1;
         
         for(int i = 1; i<nums.size();i++){
-            if(nums[i] == nums[prev] && oct%2==1){
+            if(nums[i] == nums[i-1] && oct%2==1){
                 ct++;
             }else{
                 oct++;
             }
-            prev = i;
         }
         if(oct%2 != 0){
             ct++;
