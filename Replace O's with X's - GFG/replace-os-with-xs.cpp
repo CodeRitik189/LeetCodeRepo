30fafff26aec8651e119890d9b1f8a3e08f9df87
Ritik Tripathi <88445142+CodeRitik189@users.noreply.github.com>
2022-06-30 07:43:47 +0530

    Added solution - LeetHub

---
+++ Replace O's with X's - GFG/replace-os-with-xs.cpp
@@ -0,0 +1,79 @@
+// { Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    //just 
+    void dfs(vector<vector<char>>& mat,int i, int j){
+        if(i<0||j<0||i>=mat.size()||j>=mat[0].size()||mat[i][j]!='O'){
+            return;
+        }
+        mat[i][j] = '#';
+        dfs(mat,i+1,j);
+        dfs(mat,i-1,j);
+        dfs(mat,i,j+1);
+        dfs(mat,i,j-1);
+    }
+    vector<vector<char>> fill(int n, int m, vector<vector<char>> mat)
+    {
+        // code here
+        vector<vector<int>>vis(n,vector<int>(m,0));
+        
+        for(int i = 0; i<n ; i++){
+            if(mat[i][0]=='O'){
+                dfs(mat,i,0);
+            }
+            if(mat[i][m-1]=='O'){
+                dfs(mat,i,m-1);
+            }
+        }
+        for(int i = 0; i<m ; i++){
+            if(mat[0][i]=='O'){
+                dfs(mat,0,i);
+            }
+            if(mat[n-1][i]=='O'){
+                dfs(mat,n-1,i);
+            }
+        }
+        for(int i = 0; i<n; i++){
+            for(int j = 0; j<m; j++){
+                if(mat[i][j]=='O')
+                    mat[i][j] = 'X';
+                else if(mat[i][j]=='#')
+                    mat[i][j] = 'O';
+            }
+        }
+        return mat;
+    }
+};
+
+// { Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n, m;
+        cin>>n>>m;
+        vector<vector<char>> mat(n, vector<char>(m, '.'));
+        for(int i = 0;i < n;i++)
+            for(int j=0; j<m; j++)
+                cin>>mat[i][j];
+        
+        Solution ob;
+        vector<vector<char>> ans = ob.fill(n, m, mat);
+        for(int i = 0;i < n;i++) {
+            for(int j = 0;j < m;j++) {
+                cout<<ans[i][j]<<" ";
+            }
+            cout<<"\n";
+        }
+    }
+    return 0;
+}  // } Driver Code Ends
