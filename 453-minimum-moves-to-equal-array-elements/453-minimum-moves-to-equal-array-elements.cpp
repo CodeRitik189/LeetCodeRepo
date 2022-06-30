054e7e792eef4a4491022ac55f868b7460735fef
Ritik Tripathi <88445142+CodeRitik189@users.noreply.github.com>
2022-06-30 07:00:10 +0530

    Time: 100 ms (5.95%), Space: 28.3 MB (41.83%) - LeetHub

--- 453-minimum-moves-to-equal-array-elements/453-minimum-moves-to-equal-array-elements.cpp
+++ 453-minimum-moves-to-equal-array-elements/453-minimum-moves-to-equal-array-elements.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+    //instead of incrementing (n-1) by 1 decrement that 1 
+    //beauase you can only decrement make all the elements to the smallest one
     int minMoves(vector<int>& nums) {
         long long int n = nums.size(),d2 = 0,d1 = 0,ans = INT_MAX;
         sort(nums.begin(),nums.end());
