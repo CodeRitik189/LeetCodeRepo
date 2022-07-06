8c098cbe0b203d6668f1b4c3ac85b68463f12cb8
Ritik Tripathi <88445142+CodeRitik189@users.noreply.github.com>
2022-07-06 17:27:41 +0530

    Time: 3 ms (79.68%), Space: 7.3 MB (92.12%) - LeetHub

--- 241-different-ways-to-add-parentheses/241-different-ways-to-add-parentheses.cpp
+++ 241-different-ways-to-add-parentheses/241-different-ways-to-add-parentheses.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    vector<vector<vector<int>>>dp;
     vector<int> find(string& s, int st, int endi){
         if(st==endi)return {s[st]-'0'};
         if(st==endi-1)return {(s[st]-'0')*10+(s[endi]-'0')};
@@ -7,6 +8,7 @@ public:
         vector<int>ans;
         for(int i = st+1; i<endi;i++){
             if( s[i]!='*'&&s[i]!='+'&&s[i]!='/'&&s[i]!='-')continue;
+            if(dp[st][endi].size()!=0)return dp[st][endi];
             
             vector<int>v1 = find(s,st,i-1);
             vector<int>v2 = find(s,i+1,endi);
@@ -18,9 +20,11 @@ public:
                 }
             }
         }
-        return ans;
+        return dp[st][endi] = ans;
     }
     vector<int> diffWaysToCompute(string exp) {
-       return find(exp,0,exp.size()-1);
+       int n = exp.size();
+       dp = vector<vector<vector<int>>>(n,vector<vector<int>>(n,vector<int>()));
+       return find(exp,0,n-1);
     }
 };
