31fe4c891f2ca248259c011bffb781f2b07e334e
Ritik Tripathi <88445142+CodeRitik189@users.noreply.github.com>
2022-07-07 12:25:05 +0530

    Time: 153 ms (9.83%), Space: 123.1 MB (7.06%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -1,33 +1,20 @@
 class Solution {
 public:
-    int lengthOfLongestSubstring(string s) {
-        // int i=0,j=0,ans=0;
-        // unordered_map<int,int>um;
-        // while(i<s.size()){
-        //     if(um.find(s[i])!=um.end()&&um[s[i]]>=j){
-        //         j = um[s[i]]+1;
-        //     }else{
-        //         um[s[i]] = i;
-        //         ans = max(ans,i-j+1);
-        //         i++;
-        //     }
-        // }
-        // return ans;
-        
-        int n = s.size(), ans = 0, j = 0;
-    
-        vector<int>hash(256,0);
-        
-        for(int i = 0 ; i<n;){
-                if(j<i && hash[s[i]]>0){
-                    hash[s[j]]--;
-                    j++;
-                }else{
-                    hash[s[i]]++;
-                    ans = max(ans,i-j+1);
-                    i++;
-                }     
+    int lengthOfLongestSubstring(string s) {        
+         int ans = 0;
+         
+        for(int i = 0; i<s.size(); i++){
+             bool flag = true;
+             vector<int>v(256,0);
+            for(int j = i; j<s.size();j++){
+                //entered in a substring from i to j
+                if(v[s[j]]>0){
+                    break;
+                }
+                ans = max(ans,j-i+1);
+                v[s[j]]++;
+            }
         }
-        return ans;      
+        return ans;
     }
 };
