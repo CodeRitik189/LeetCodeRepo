be4abf6c0518307a3f2f330eb805cdebe4d53899
Ritik Tripathi <88445142+CodeRitik189@users.noreply.github.com>
2022-07-07 09:32:19 +0530

    Time: 4 ms (73.94%), Space: 6.8 MB (58.84%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
-    vector<vector<int>>dp;
-    bool check(string& s1, string& s2, string& s3, int i, int j, int k){
-        if(i<0 || j<0){
-            if(k<0)return (i<0&&j<0);
-            else if(i>=0)return (s1[i]==s3[k]&&check(s1,s2,s3,i-1,j,k-1));
-            else if(j>=0)return (s2[j]==s3[k]&&check(s1,s2,s3,i,j-1,k-1));
-            return false;  
-        }
-    if(dp[i][j]!=-1)return dp[i][j];  
-    if(s1[i]!=s3[k] && s2[j]!=s3[k])return dp[i][j] = false;
-        
-  return dp[i][j] = (s1[i]==s3[k]&&check(s1,s2,s3,i-1,j,k-1))||(s2[j]==s3[k]&&check(s1,s2,s3,i,j-1,k-1));
-    }
     bool isInterleave(string s1, string s2, string s3) {
         int x = s1.size(),y = s2.size(),z = s3.size();
-        dp = vector<vector<int>>(x,vector<int>(y,-1));
-        return x+y==z&&check(s1,s2,s3,x-1,y-1,z-1);
+        if(x+y!=z)return false;
+        
+        vector<int>dp(y+1,0);
+        dp[0] = 1;
+        for(int i = 1; i<= y; i++)dp[i] = s3[i-1]==s2[i-1]&&dp[i-1];
+        
+        for(int i=1; i<= x; i++){
+            vector<int>temp(y+1,0);
+            temp[0] = (s3[i-1]==s1[i-1])&&dp[0];
+            
+            for(int j = 1; j<= y; j++){
+                temp[j] = (s3[i+j-1] == s1[i-1] && dp[j])||(s3[i+j-1] == s2[j-1] && temp[j-1]);
+            }
+            dp = temp;
+        }
+        return dp[y];
     }
 };
