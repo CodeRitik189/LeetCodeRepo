fc6d928de0f4ac5ab4d12d59dbe8056c585066e1
Ritik Tripathi <88445142+CodeRitik189@users.noreply.github.com>
2022-07-08 11:04:12 +0530

    Time: 48 ms (17.49%), Space: 16.9 MB (85.97%) - LeetHub

--- 136-single-number/136-single-number.cpp
+++ 136-single-number/136-single-number.cpp
@@ -1,30 +1,26 @@
 class Solution {
 public:
-    //log(n) code for sorted array
-   /* int check(vector<int>& nums,int st,int endi){
-        if(st>endi){return -1;}
-        int mid = st+(endi-st)/2;
-        if((nums[mid]!=nums[mid-1])&&(nums[mid]!=nums[mid+1])){
-            return nums[mid];
-        }else if(nums[mid]==nums[mid-1]){
-            if(mid%2==0){
-                return check(nums,st,mid-1);
-            }else{
-                return check(nums,st,mid+1);
-            }
-        }else{
-            if(mid%2==0){
-                return check(nums,st,mid+1);
-            }else{
-                return check(nums,st,mid-1);
-            }
-        }
-    }*/
+    // 1 1 2 2 3 4 4
+    // 0 1 2 3 4 5 6 
+    
     int singleNumber(vector<int>& nums) {
-        int xr = 0;
-        for(int i=0;i<nums.size();i++){
-            xr ^= nums[i];
+        sort(nums.begin(),nums.end());
+        if(nums.size()==1)return nums[0];
+        
+        int st = 0, endi = nums.size()-1;
+      while(st<= endi){
+        int mid = st+(endi-st)/2;
+        
+        if(mid<nums.size()-1 && nums[mid]==nums[mid+1]){
+            if(mid%2==0)st = mid+1;
+            else endi = mid-1;
+        }else if(mid>0 && nums[mid]==nums[mid-1]){
+            if(mid%2!=0)st = mid+1;
+            else endi = mid-1;
+        }else{
+            return nums[mid];
         }
-        return xr;
+      }
+        return -1;
     }
 };
