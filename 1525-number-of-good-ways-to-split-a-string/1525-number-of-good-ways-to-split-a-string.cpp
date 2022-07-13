99c2739ad41bcc345da2c1c95b9d9a5e6c15449c
Ritik Tripathi <88445142+CodeRitik189@users.noreply.github.com>
2022-07-13 23:41:48 +0530

    Time: 58 ms (46.10%), Space: 10.3 MB (35.79%) - LeetHub

--- 1525-number-of-good-ways-to-split-a-string/1525-number-of-good-ways-to-split-a-string.cpp
+++ 1525-number-of-good-ways-to-split-a-string/1525-number-of-good-ways-to-split-a-string.cpp
@@ -12,10 +12,10 @@ public:
             if(um[s[i]]==1)ct++;
         }
         
-        unordered_map<int,int>um2; ct = 0;
+        um.clear(); ct = 0;
         for(int i = 0; i<n-1; i++){
-            um2[s[i]]++;
-            if(um2[s[i]]==1)ct++;
+            um[s[i]]++;
+            if(um[s[i]]==1)ct++;
             if(ct == count[i])
                 ans++;
         }
